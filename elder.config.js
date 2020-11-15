d3b3ef50eecfe61397bb2bb3adf7d1adb065ed09
Apollinaire <no-reply@apollinai.re>
2020-11-15 15:50:59 +0100

    change env

--- elder.config.js
+++ elder.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   origin: process.env.URL || '', // TODO: update this.
   srcDir: 'src',
-  distDir: process.env.NODE_ENV === 'production' ? 'public' : 'tmp',
+  distDir: process.env.NETLIFY ? 'public' : 'tmp',
   rootDir: process.cwd(),
   build: {},
   server: {
