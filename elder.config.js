e339b407dbec95564d7cc2eb5b0cea58fe9e4fd4
Apollinaire <no-reply@apollinai.re>
2020-11-15 12:08:15 +0100

    remove images plugin

--- elder.config.js
+++ elder.config.js
@@ -25,14 +25,6 @@ module.exports = {
       // this reloads your browser when nodemon restarts your server.
       port: 8080,
     },
-    '@elderjs/plugin-images': {
-      folders: [
-        {
-          src: '/images/*', // glob of where your original images are. Relative to rootDir/process.cwd() defined in your elder.config.js. Careful with **.
-          output: '/images/', // where files should be put within the distDir defined in your elder.config.js.
-        },
-      ],
-    },
     'elderjs-plugin-subfont': {},
   },
   shortcodes: { closePattern: '}}', openPattern: '{{' },
